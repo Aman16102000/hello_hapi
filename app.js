84744dd940d93cdb274426e1f21fdc60878e8cd6
aman.thakur <aman.thakur>
2023-11-19 20:10:32 +0530

    minor fix

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ if (!module.parent) {
             throw err;
         }
 
-        console.log(`Server running at: ${Server.info.uri}`);
+        console.log(`Server running at the: ${Server.info.uri}`);
     });
 }
 
